db74f7a12789129b790dcda166db3b33e3d2f327
arroyostack <arroyostack@gmail.com>
2022-07-19 22:36:50 +0200

    Font changes, percentages included

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,9 @@
     <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
       <div class=" rounded-lg px-4 py-6 md:py-8 lg:py-12">
         <img src="../assets/elhadj.png" class="logo-image ml-7" alt="" />
+        <br>
         <h2
-          class=" text-slate-300 text-4xl lg:text-6xl  ml-7 mb-4 md:mb-6 "
+          class=" text-white font-medium text-4xl lg:text-6xl  ml-7 mb-4 md:mb-6 "
         >
           El Hadj Arroyo
         </h2>
@@ -17,29 +18,29 @@
             font-medium ml-7 mx-auto italic">
           'JavaScript developers are at the heart of what may be the single
           biggest revolution in the history of computing: the dawn of the
-          realtime web('Programming JavaScript Applications', Eric Elliott)'
+          realtime web (' Programming JavaScript Applications', Eric Elliott ')
         </p>
         <br />
-        <p class="max-w-screen-md text-slate-300 md:text-lg ml-7 mx-auto">
+        <p class="max-w-screen-md text-white font-medium md:text-lg ml-7 mx-auto">
           Since turning my passion for technology into a career three years ago,
           I have been involved in professional Wordpress web developing projects
           while building my own Javascript apps.
         </p>
         <br />
-        <p class="max-w-screen-md text-slate-300 md:text-lg ml-7 mx-auto">
+        <p class="max-w-screen-md text-white font-medium md:text-lg ml-7 mx-auto">
           Exposure to different projects brought an increasingly interest for
           Javascript and web technologies. This has led me to deep dive into not
           only Javascript, HTML and CSS principles, but also frameworks like Vue
           3, CSS libraries like Sass and backend applications like Firebase.
         </p>
         <br />
-        <p class="max-w-screen-md text-slate-300 md:text-lg ml-7 mx-auto">
+        <p class="max-w-screen-md text-white font-medium md:text-lg ml-7 mx-auto">
           At the present keep reinforcing my knowledge in Web developing and Javascript
           related technology and best practices as well as digging deeper into data structures and
           algorithms.
         </p>
         <br />
-        <p class="max-w-screen-md text-slate-300 md:text-lg ml-7 mx-auto font-medium">
+        <p class="max-w-screen-md text-white font-medium md:text-lg ml-7 mx-auto font-medium">
            Looking forward to join a team and build real world apps.
         </p>
       </div>
